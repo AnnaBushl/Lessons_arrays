5c7bb944e868439aa323e60d8efb6cd778cfdab1
Ann <ann.bushl2444@gmail.com>
2022-05-09 12:05:18 +0300

    Initial commit

---
+++ src/task1.java
@@ -0,0 +1,20 @@
+public class task1 {
+
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i=0; i< arr.length; i++) {
+            sum+= arr[i];
+        }
+        System.out.println(sum);
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+}
